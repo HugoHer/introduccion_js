a569e6e8040a3c51d10b48a5d3be6861504b5da4
Hugo Hernandez <nandezhugo86@gmail.com>
2022-08-03 00:07:35 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+/*
+6 / "3"       2
+"2" * "3"     6
+4 + 5 + "px" 9px
+"$" + 4 + 5    $45
+"4" - 2         2
+"4px" - 2        nan
+7 / 0            0
+{}[0]            0
+parseInt("09")    9
+5 && 2            1
+2 && 5            0
+5 || 0            true
+0 || 5            false
+[3]+[3]-[10]        -4  
+3>2>1               false
+[] == ![]            false
+
+var profesor = "Jhoswe";
+let teacher = "Jose";
+if (true) {
+    var profesor = "The Flash";
+    let teacher = "Reverse Flash";
+    console.log(profesor);       the flash
+    console.log(teacher);          reverse flash
+}
+console.log(profesor);        "jhoswe"
+console.log(teacher);           "jose"
+*/
